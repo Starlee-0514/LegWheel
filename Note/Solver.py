b81185ca6ca1277706062c4c22c1b6256d186c5a
Starlee-0514 <starlee0514@gmail.com>
2025-08-29 18:34:54 +0800

    adjust animation script add equation solver for some functions

---
+++ Note/Solver.py
@@ -0,0 +1,117 @@
+import numpy as np
+
+class Solver:
+    """
+    An optimization solver for equations
+    Args:
+        method (str): The optimization method to use (default: "Newton").
+        tol (float): The tolerance for convergence (default: 1e-6).
+        max_iter (int): The maximum number of iterations (default: 100).
+        function (callable): The function to optimize (default: None).
+        derivative (callable): The derivative of the function (default: None).
+    """
+    def __init__(self, method = "Newton", tol=1e-6, max_iter=100, function=None, derivative=None):
+        self.method = method
+        self.tol = tol
+        self.max_iter = max_iter
+        self.function = function
+        self.derivative = derivative
+        self.method_list = ["Newton", "Bisection", "Secant", "Regula Falsi"]
+
+    def solve(self, x0, x1=None):
+        if self.method == "Newton":
+            return self.newton_method(x0)
+        elif self.method == "Bisection":
+            return self.bisection_method(x0, x1)
+        elif self.method == "Secant":
+            return self.secant_method(x0, x1)
+        elif self.method == "Regula Falsi":
+            return self.regula_falsi_method(x0, x1)
+        else:
+            raise ValueError("Unknown method: {}".format(self.method))
+
+    def newton_method(self, x0):
+        for i in range(self.max_iter):
+            if self.derivative is None:
+                raise ValueError("Derivative is not defined.")
+
+            x1 = x0 - self.function(x0) / self.derivative(x0)
+            if abs(x1 - x0) < self.tol:
+                return x1
+            x0 = x1
+        raise ValueError("Failed to converge.")
+    def bisection_method(self, x0, x1=None):
+        if x1 is None:
+            x1 = x0 + 1
+        if self.function is None:
+            raise ValueError("Function is not defined.")
+
+        for i in range(self.max_iter):
+            if self.function(x0) * self.function(x1) > 0:
+                raise ValueError("Function values at the endpoints must have opposite signs.")
+
+            x2 = (x0 + x1) / 2
+            if abs(x2 - x0) < self.tol:
+                return x2
+
+            if self.function(x2) == 0:
+                return x2
+            elif self.function(x0) * self.function(x2) < 0:
+                x1 = x2
+            else:
+                x0 = x2
+
+        raise ValueError("Failed to converge.")
+
+    def secant_method(self, x0, x1):
+        if self.function is None:
+            raise ValueError("Function is not defined.")
+        if x1 is None:
+            x1 = x0 + 1
+
+        for i in range(self.max_iter):
+            if abs(x1 - x0) < self.tol:
+                return x1
+
+            x2 = x1 - self.function(x1) * (x1 - x0) / (self.function(x1) - self.function(x0))
+            x0, x1 = x1, x2
+
+        raise ValueError("Failed to converge.")
+
+    def regula_falsi_method(self, x0, x1):
+        if self.function is None:
+            raise ValueError("Function is not defined.")
+        if x1 is None:
+            x1 = x0 + 1
+        for i in range(self.max_iter):
+            if self.function(x0) * self.function(x1) > 0:
+                raise ValueError("Function values at the endpoints must have opposite signs.")
+
+            x2 = x1 - self.function(x1) * (x1 - x0) / (self.function(x1) - self.function(x0))
+            if abs(x2 - x1) < self.tol:
+                return x2
+
+            if self.function(x2) == 0:
+                return x2
+            elif self.function(x0) * self.function(x2) < 0:
+                x1 = x2
+            else:
+                x0 = x2
+
+        raise ValueError("Failed to converge.")
+
+    def get_method_list(self):
+        return self.method_list
+
+if __name__ == "__main__":
+    # Example usage
+    # solve: x^2 = 2
+    solver = Solver(
+        method="Newton",
+        tol=1e-6,
+        max_iter=100,
+        function=lambda x: x**2 - 2,
+        derivative=lambda x: 2*x
+    )
+    root = solver.solve(1.0)
+    print("Root found:", root)
--- Note/animation.py
+++ Note/animation.py
@@ -29,25 +29,25 @@ fig, ax = plt.subplots()
 # ax = plot_leg.plot_by_angle(np.deg2rad(17), np.deg2rad(0))
 
 
-# plot leg with different theta
-def update(frame):
-    ax.clear()
-    plot_leg.plot_by_angle(np.deg2rad(frame), np.deg2rad(0), ax=ax)
-    plot_point(ax, plot_leg.rim_point(10), color='red')
-    # plot_leg.setting(mark_size=10, line_width=3, color='red')
-    ax.grid()
-    ax.set_title(f'Theta: {frame}°')
-ani = FuncAnimation(fig, update, frames=range(17, 161, 5), interval=100)
-
-# # plot rim point at leg
-# def update(frame, theta=100):
+# # plot leg with different theta
+# def update(frame):
 #     ax.clear()
-#     plot_leg.plot_by_angle(np.deg2rad(theta), np.deg2rad(0), ax=ax)
+#     plot_leg.plot_by_angle(np.deg2rad(frame), np.deg2rad(0), ax=ax)
+#     plot_point(ax, plot_leg.rim_point(10), color='red')
 #     # plot_leg.setting(mark_size=10, line_width=3, color='red')
-#     plot_point(ax, plot_leg.rim_point(frame), color='blue')
 #     ax.grid()
-#     ax.set_title(f'Theta: {theta}°, Alpha: {frame}°')
-# ani = FuncAnimation(fig, update, frames=range(-180, 180, 3), interval=1)
+#     ax.set_title(f'Theta: {frame}°')
+# ani = FuncAnimation(fig, update, frames=range(17, 161, 5), interval=100)
+
+# plot rim point at leg
+def update(frame, theta=100):
+    ax.clear()
+    plot_leg.plot_by_angle(np.deg2rad(theta), np.deg2rad(0), ax=ax)
+    # plot_leg.setting(mark_size=10, line_width=3, color='red')
+    plot_point(ax, plot_leg.rim_point(frame), color='blue')
+    ax.grid()
+    ax.set_title(f'Theta: {theta}°, Alpha: {frame}°')
+ani = FuncAnimation(fig, update, frames=range(-180, 180, 3), interval=1)
 
 
 plt.show()
